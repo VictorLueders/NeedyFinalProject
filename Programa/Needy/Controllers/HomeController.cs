60196d71af95e16844f2f88435d982a6867d3183
BrunoKnaesel <knaeselbruno@gmail.com>
2021-02-09 17:01:29 -0300

    BOTOES

--- Programa/Needy/Controllers/HomeController.cs
+++ Programa/Needy/Controllers/HomeController.cs
@@ -45,6 +45,12 @@ namespace Needy.Controllers
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Fire()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()

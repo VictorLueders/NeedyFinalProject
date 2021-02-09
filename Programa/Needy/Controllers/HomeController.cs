f717ee8cd549155d4d124dcd18e9bc69ba79babe
BrunoKnaesel <knaeselbruno@gmail.com>
2021-02-09 16:36:03 -0300

    First commit

---
+++ Programa/Needy/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Needy.Models;
+using System.Diagnostics;
+
+namespace Needy.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Emergency(string nome, string cpf, string rg)
+        {
+            ViewBag.Nome = nome;
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Crimes()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Waiting()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Programa/Needy/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Needy.Models
+{
+    public class Usuario
+    {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Rg { get; set; }
+
+    }
+}
